67b32c3e3614559d603eb056b3c4896f67dea97c
keiichi3573 <kei.action-up@m.speedia.jp>
2026-09-23 15:48:24 +0900

    Update duplicate-customers.js

--- duplicate-customers.js
+++ duplicate-customers.js
@@ -1,3 +1,9 @@
+const sb =
+  supabase.createClient(
+    window.APP_CONFIG.SUPABASE_URL,
+    window.APP_CONFIG.SUPABASE_ANON_KEY
+  );
+
 const duplicateCustomersApp =
   document.getElementById(
     "duplicateCustomersApp"
